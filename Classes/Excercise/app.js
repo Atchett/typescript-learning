272db1f2a2c792392397a554234858a41fb0438f
John Spurgin <johnspurgin@gmail.com>
2019-06-26 20:16:16 +0100

    completing the excercises

--- Classes/Excercise/app.js
+++ Classes/Excercise/app.js
@@ -3,6 +3,19 @@
 // function Car(name) {
 // 	this.name = name;
 // 	this.acceleration = 0;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // 	this.honk = function() {
 // 		console.log("Toooooooooot!");
 // 	};
@@ -51,22 +64,7 @@
 // console.log(person.firstName);
 // person.firstName = "Maximilian";
 // console.log(person.firstName);
-// Exercise 1 - How was your TypeScript Class?
-// function Car(name) {
-// 	this.name = name;
-// 	this.acceleration = 0;
-// 	this.honk = function() {
-// 		console.log("Toooooooooot!");
-// 	};
-// 	this.accelerate = function(speed) {
-// 		this.acceleration = this.acceleration + speed;
-// 	}
-// }
-// var car = new Car("BMW");
-// car.honk();
-// console.log(car.acceleration);
-// car.accelerate(10);
-// console.log(car.acceleration);
+// // Exercise 1 - How was your TypeScript Class?
 var Car = /** @class */ (function () {
     function Car(name) {
         this.acceleration = 0;
@@ -87,3 +85,50 @@ car.accelerate(10);
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
+// // Exercise 2 - Two objects, based on each other ...
+var BaseObject = /** @class */ (function () {
+    function BaseObject(width, lenght) {
+        this.width = 0;
+        this.lenght = 0;
+        this.width = width;
+        this.lenght = lenght;
+    }
+    return BaseObject;
+}());
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Rectangle.prototype.calcSize = function () {
+        return this.width * this.lenght;
+    };
+    return Rectangle;
+}(BaseObject));
+var rect = new Rectangle(10, 20);
+console.log("Rect : " + rect.calcSize());
+// // Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+var PersonA = /** @class */ (function () {
+    function PersonA() {
+        this._firstName = "";
+    }
+    Object.defineProperty(PersonA.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._firstName = value;
+            }
+            else {
+                this._firstName = "Enter name";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return PersonA;
+}());
+var personA = new PersonA();
+personA.firstName = "Joh";
+console.log(personA.firstName);
--- Classes/Excercise/app.ts
+++ Classes/Excercise/app.ts
@@ -55,28 +55,8 @@
 // person.firstName = "Maximilian";
 // console.log(person.firstName);
 
-
-
-
-// Exercise 1 - How was your TypeScript Class?
-// function Car(name) {
-// 	this.name = name;
-// 	this.acceleration = 0;
 	
-// 	this.honk = function() {
-// 		console.log("Toooooooooot!");
-// 	};
-	
-// 	this.accelerate = function(speed) {
-// 		this.acceleration = this.acceleration + speed;
-// 	}
-// }
-// var car = new Car("BMW");
-// car.honk();
-// console.log(car.acceleration);
-// car.accelerate(10);
-// console.log(car.acceleration);
-	
+// // Exercise 1 - How was your TypeScript Class?
 class Car {
 	name: string;
 	acceleration: number = 0;
@@ -93,10 +73,55 @@ class Car {
 		this.acceleration = this.acceleration + speed;
 	}
 }
-var car = new Car("BMW");
+const car = new Car("BMW");
 car.honk();
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
+
+	
+// // Exercise 2 - Two objects, based on each other ...
+abstract class BaseObject {
+	width: number = 0;
+	lenght: number = 0;
+
+	constructor(width: number, lenght: number) {
+		this.width = width;
+		this.lenght = lenght;
+	}
+
+	abstract calcSize():number;
+}
+
+class Rectangle extends BaseObject {
+
+	calcSize():number {
+		return this.width * this.lenght;
+	}
+}
+
+const rect = new Rectangle(10, 20);
+console.log("Rect : " + rect.calcSize());
+
+// // Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+class PersonA {
+	private _firstName: string = "";
+
+	set firstName(value:string) {
+		if (value.length > 3) {
+			this._firstName = value;
+		} else {
+			this._firstName = "Enter name";
+		}
+	}
+
+	get firstName():string {
+		return this._firstName;
+	}
+}
+
+const personA = new PersonA();
+personA.firstName = "Joh";
+console.log(personA.firstName);
