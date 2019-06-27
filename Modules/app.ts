6e055e5adf9c7fefe9a884bd97fb5bc938e40df7
John Spurgin <johnspurgin@gmail.com>
2019-06-27 14:30:23 +0100

    working through examples

--- Modules/app.ts
+++ Modules/app.ts
@@ -1,5 +1,13 @@
-import { PI, calculateCircumference } from "./math/circle";
+//import { PI, calculateCircumference } from "./math/circle";
+// or
+import * as Circle from "./math/circle";
+// import {calculateRectangle} from "./math/rectangle"; // when not using default in the file
+import calcRec from "./math/rectangle";
 
-console.log(PI);
-console.log(calculateCircumference(10));
-// console.log();
+// console.log(PI);
+// console.log(calculateCircumference(10));
+// or
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+//console.log(calculateRectangle(10, 2));
+console.log(calcRec(10, 2));
