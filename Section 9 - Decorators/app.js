77bc84b5e0b5cf3f38328d2c234bddc3070501fb
John Spurgin <johnspurgin@gmail.com>
2019-07-06 16:56:11 +0100

    working through examples

--- Section 9 - Decorators/app.js
+++ Section 9 - Decorators/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logged(constructorFn) {
     console.log(constructorFn);
 }
@@ -22,6 +25,8 @@ var Person = /** @class */ (function () {
 function logging(value) {
     return value ? logged : function () { };
 }
+// attach the result of the function 
+// for execution
 var Car = /** @class */ (function () {
     function Car() {
     }
@@ -30,3 +35,90 @@ var Car = /** @class */ (function () {
     ], Car);
     return Car;
 }());
+// Advanced decorator
+// each object created or instantiated
+// based on a class which has the printable decorator
+// will automatically receive the print method
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = "green plant";
+    }
+    Plant = __decorate([
+        logging(true),
+        printable
+    ], Plant);
+    return Plant;
+}());
+var plant = new Plant();
+// explicitly cast to type any // bug
+plant.print();
+// Method decorator
+// Property decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+// should really be used as a listener
+// read and react to values, not locking
+function overwriteable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    Project.prototype.calcBudget = function () {
+        console.log(1000);
+    };
+    __decorate([
+        overwriteable(false)
+    ], Project.prototype, "projectName", void 0);
+    __decorate([
+        editable(false)
+    ], Project.prototype, "calcBudget", null);
+    return Project;
+}());
+// commented out as correctly thowing an error, but blocks the JS thread
+// const project = new Project("Super Project");
+// project.calcBudget();
+// project.calcBudget = function() {
+// 	console.log(2000);
+// }
+// project.calcBudget();
+// Parameter decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
+}());
+var course = new Course("Great Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
--- Section 9 - Decorators/app.ts
+++ Section 9 - Decorators/app.ts
@@ -15,7 +15,101 @@ function logging(value: boolean) {
 	return value ? logged : () => {};
 }
 
+// attach the result of the function 
+// for execution
 @logging(true)
 class Car {
 
 }
+
+// Advanced decorator
+
+// each object created or instantiated
+// based on a class which has the printable decorator
+// will automatically receive the print method
+function printable(constructorFn: Function) {
+	constructorFn.prototype.print = function() {
+		console.log(this);
+	}
+}
+
+@logging(true)
+@printable
+class Plant {
+	name: string = "green plant";
+}
+
+const plant = new Plant();
+// explicitly cast to type any // bug
+(<any>plant).print();
+
+
+// Method decorator
+// Property decorator
+function editable(value: boolean) {
+	return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+		descriptor.writable = value;
+	}
+}
+
+// should really be used as a listener
+// read and react to values, not locking
+function overwriteable(value: boolean) {
+	return function(target: any, propName: string): any {
+		const newDescriptor: PropertyDescriptor = {
+			writable: value
+		};
+		return newDescriptor;
+	}
+}
+
+class Project {
+	@overwriteable(false)
+	projectName: string;
+
+	constructor(name: string) {
+		this.projectName = name;
+	}
+
+	@editable(false)
+	calcBudget() {
+		console.log(1000);
+	}
+}
+
+// commented out as correctly thowing an error, but blocks the JS thread
+ 
+// const project = new Project("Super Project");
+// project.calcBudget();
+// project.calcBudget = function() {
+// 	console.log(2000);
+// }
+// project.calcBudget();
+
+
+
+// Parameter decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+	console.log("Target: ", target);
+	console.log("methodName: ", methodName);
+	console.log("paramIndex: ", paramIndex);
+}
+
+class Course {
+	name: string;
+	constructor(name: string) {
+		this.name = name;
+	}
+
+	printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+		if (printAll) {
+			console.log(10000);
+		} else {
+			console.log(2000);
+		}
+	}
+}
+
+const course = new Course("Great Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
