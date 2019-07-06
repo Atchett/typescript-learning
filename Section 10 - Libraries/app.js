c119f0c30170efa6bb820bee3b0961f47820f047
John Spurgin <johnspurgin@gmail.com>
2019-07-06 20:54:41 +0100

    working through examples

---
+++ Section 10 - Libraries/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1 - not the best way to declare it
+// declare var $: any;
+// 2 - declare using declaration files something.d.ts
+// 3 - install type definition using npm - 
+// npm init
+// run through setup of package.json file
+// npm install --save-dev @types/jquery
+$('button').click(function () {
+    alert('botton clicked');
+});
